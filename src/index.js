05239c3dc1fe0692a95acf5ac2d72c120627043c
Roi Driscoll <roi.driscoll@gmail.com>
2018-12-12 16:26:54 +0000

    Adds Sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./css-reset.css";
-import "./index.css";
+import "./css-reset.scss";
+import "./index.scss";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
