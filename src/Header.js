5bb38120a44a0e02778cb04c572ce7bedaa10d8f
Roi Driscoll <roidriscoll@RDriscoll.domain.name>
2018-12-16 15:07:50 +0000

    Starting to extract out React components

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Header.scss";
+
+const Header = props => {
+  return (
+    <header class="creator-information">
+      <h1 class="creator-information__name">NBA</h1>
+    </header>
+  );
+};
+
+export default Header;
